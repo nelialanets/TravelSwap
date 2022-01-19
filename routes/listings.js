da4a5418cef4133ce56d770f0a59541cb313a7b7
trevorwrightdev <trevor.wright.710@gmail.com>
2022-01-19 16:14:56 -0700

    Formatted all routes for editing and deleting

--- routes/listings.js
+++ routes/listings.js
@@ -6,9 +6,9 @@ router.get('/', listilngsCTRL.index);
 
 router.get('/newlisting', listilngsCTRL.newListing);
 router.post('/', listilngsCTRL.postListing);
-// router.get('/newlisting',listilngsCTRL.newListing);
-// router.get('/:id', listilngsCTRL.show);
-// router.get('/:id/edit', listilngsCTRL.edit);
-// router.delete('/:id',listilngsCTRL.remove)
+router.get('/:id', listilngsCTRL.show);
+router.get('/:id/edit', listilngsCTRL.edit);
+router.put('/:id', listilngsCTRL.updateListing);
+router.delete('/:id',listilngsCTRL.remove)
 
 module.exports = router;
